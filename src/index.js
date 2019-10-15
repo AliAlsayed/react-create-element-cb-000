cdbb6728332d3dd56e8331cab55aed3c5d1fc9fc
Ali Alsayed <a_alsayed87@hotmail.com>
2019-10-15 08:30:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = React.createElement('h1', {}, 'My First React Code');
-const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
-const container = React.createElement('div', {}, [title, paragraph]);
-
+const list =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'My favorite ice cream flavors'),
+    React.createElement('ul', {},
+      [
+        React.createElement('li', {}, 'Chocolate'),
+        React.createElement('li', {}, 'Vanilla'),
+        React.createElement('li', {}, 'Banana')
+      ]
+    )
+  );
 ReactDOM.render(
   container,
   document.getElementById('global')
