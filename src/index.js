109ae6e0e4369e71595f6c0b9d92e46954cdd187
Ali Alsayed <a_alsayed87@hotmail.com>
2019-10-15 08:26:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const title = React.createElement('h1', {}, 'My First React Code');
+const container = React.createElement('div', {}, title);
 
 ReactDOM.render(
   title,
